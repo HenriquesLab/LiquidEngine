4748faa742bb847e5ba1a6c361cee99ddb701ee7
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-06-05 10:52:56 +0100

    added automatic doc generation

--- src/liquid_engine/__init__.py
+++ src/liquid_engine/__init__.py
@@ -1,2 +1,12 @@
 from .liquid_engine import LiquidEngine
 from .__agent__ import Agent
+
+__all__ = [
+    "liquid_engine",
+    "__agent__",
+    "__cuda__",
+    "__dask__",
+    "__njit__",
+    "__transonic__",
+    "__opencl__",
+]
