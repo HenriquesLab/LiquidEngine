b7d0eb548e5368c898036a5a1a8f1fc7de053830
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2024-06-28 15:55:11 +0100

    improved testing

--- tests/test_le.py
+++ tests/test_le.py
@@ -1,23 +1,43 @@
+import time
 from liquid_engine import LiquidEngine
 
 
 class MyLE(LiquidEngine):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self._designation = "MyLE"
-        super().__init__()
+        super().__init__(*args, **kwargs)
 
-    def run(self):
-        return self._run()
+    def run(self, var, run_type=None):
+        return self._run(var, run_type=run_type)
 
-    def _run_a(self):
+    def _run_a(self, var):
+        time.sleep(0.1)
         return 1.23
 
-    def _run_b(self):
-        return 4.56
+    def _run_b(self, var):
+        time.sleep(1)
+        return 1.23
+
+    def _run_c(self, var):
+        return asdas  #forcing an error to be handled by the LE
+
+    def _compare_runs(self, a, b):
+        return a == b
 
 
 def test_myle():
-    le = MyLE()
-    le.run()
-    le.benchmark()
-    le.run()
+    le = MyLE(testing=False)
+    le.benchmark("var_2")
+    le.benchmark("var_2")
+    le.benchmark("var_2")
+    le.run("var_2", run_type="c")
+    le.run("var_2")
+
+
+def test_myle_testing():
+    le = MyLE(testing=True)
+    le.benchmark("var_2")
+    le.benchmark("var_2")
+    le.benchmark("var_2")
+    le.run("var_2", run_type="c")
+    le.run("var_2")
